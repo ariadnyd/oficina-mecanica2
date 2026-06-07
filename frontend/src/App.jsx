4536f6098aea064f6b52e91221ed4de4e5a8225b
Ariadny Francisca Dantas Santos <ariadnyfran15@gmail.com>
2026-06-07 16:13:38 -0300

    feat: atualizando a tela principal com o caminho para Clientes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import TelaClientes from './pages/TelaClientes';
+import './App.css';
 
 function App() {
   return (
-    <main>
-      <h1>Sistema da Oficina Mecânica 🚗</h1>
-      <h2>Módulo de Clientes</h2>
-      
-      {/* Aqui vai entrar a nossa lista de clientes em breve! */}
-      <p>Nenhum cliente carregado ainda.</p>
-    </main>
-  )
+    <BrowserRouter>
+      <main>
+        <h1>Sistema da Oficina Mecânica 🚗</h1>
+        
+        {/* Um menu de navegação simples */}
+        <nav style={{ marginBottom: '30px', padding: '10px', backgroundColor: 'var(--social-bg)', borderRadius: '8px' }}>
+          <Link to="/clientes" style={{ fontWeight: 'bold' }}>Ver Clientes</Link>
+        </nav>
+
+        {/* O "GPS" que decide qual tela renderizar no meio da página */}
+        <Routes>
+          <Route path="/clientes" element={<TelaClientes />} />
+          {/* Futuramente, quando criar a OS, é só adicionar a rota dela aqui embaixo! */}
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
